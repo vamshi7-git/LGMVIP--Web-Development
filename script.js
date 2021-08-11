dc4c7a1bf93d33e9f5a96a112758ff1b67def553
VamshiKrishna <vamshikalagara777@gmail.com>
2021-08-12 00:32:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ var menu = document.querySelector("#menu-bar");
 var navbar = document.querySelector(".navigationbar");
 
 menu.addEventListener('click', () => {
-    menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
 });
