ea9b379eea4128bb3b3b5d4272ffde60cfa0bb93
VamshiKrishna <vamshikalagara777@gmail.com>
2021-08-17 23:19:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var menu = document.querySelector("#menu-bar");
-var navbar = document.querySelector(".navigationbar");
-
-menu.addEventListener('click', () => {
-    navbar.classList.toggle("active");
-});
